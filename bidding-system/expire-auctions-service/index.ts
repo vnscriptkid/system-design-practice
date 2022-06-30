18248121654926b04f9f3ed13eb97a0b52a25ad6
Thanh Nguyen <vnscriptkid@gmail.com>
2022-06-30 18:44:28 +0700

    feat(bidding-system): expire auctions service

---
+++ bidding-system/expire-auctions-service/index.ts
@@ -0,0 +1,21 @@
+import { connectDb, pool } from "./db";
+var cron = require("node-cron");
+
+async function main() {
+  await connectDb();
+
+  cron.schedule("* * * * *", async () => {
+    console.info(`@@ process auctions every minute!`)
+
+    const nowInSec = ~~(Date.now() / 1000)
+    
+    const {rowCount, rows} = await pool.query({
+      text: `update auctions set status = 'CLOSED' where ended_at <= $1 and status = 'OPEN' returning *;`,
+      values: [nowInSec]
+    })
+
+    console.info(`^^ expired ${rowCount} auction(s)`, [rows.map(x => x.id)])
+  });
+}
+
+main();
